e41a57198369627a51c1faa196a1ee70c4e4327e
DAVI CUSTÓDIO DA SILVA <davi.silva328@senaisp.edu.br>
2026-05-21 15:41:27 -0300

    Arrumando links de backend

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://c312-2804-868-d057-4274-2886-64c9-196e-1bb6.ngrok-free.app"
+  baseURL: "http://localhost:3000"
 });
 
 export default api;
