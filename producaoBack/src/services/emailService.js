9e6664e8f155e61bb558a363ee7e949b967ec694
DAVI CUSTÓDIO DA SILVA <davi.silva328@senaisp.edu.br>
2026-05-25 13:08:14 -0300

    Arrumando consfigurações do nodeMailer e arrumando tela para reenvio do email e token

--- producaoBack/src/services/emailService.js
+++ producaoBack/src/services/emailService.js
@@ -6,8 +6,8 @@ export const enviarEmailVerificacao = async (email, link) => {
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
 
     auth: {
       user: process.env.EMAIL_USER,
